0a1d9163baa5ec39cd318d4168e6f308c80a88de
Tim Gorringe <tgorringe4@gmail.com>
2018-02-09 15:27:14 +0000

    Beta banner and reset code for verification attempts

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,6 @@ module.exports = router;
 
 router.get('/challenge/verify/attempt-handler/', function (req, res) {
     
-    
     var result = attempt++;
     
     console.log(result);
@@ -16,4 +15,14 @@ router.get('/challenge/verify/attempt-handler/', function (req, res) {
  else {
     res.redirect('nextGo');
   }
+});
+
+router.get('/', function (req, res) {
+  attempt = 0;
+  res.render('index');
+});
+
+router.get(/index/, function (req, res) {
+  attempt = 0;
+  res.render('index');
 });

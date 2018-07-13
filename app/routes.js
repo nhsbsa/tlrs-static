1d30bbe8fd1f0e886f4701a8b2fdca22da26cebc
Tim Gorringe <tgorringe4@gmail.com>
2018-07-13 15:17:48 +0100

    updated end page content and did some work on payment journeys

--- app/routes.js
+++ app/routes.js
@@ -26,3 +26,34 @@ router.get(/index/, function (req, res) {
   attempt = 0;
   res.render('index');
 });
+
+router.get(/instalment-handler/, function (req, res) {
+  if (req.query.plan == "2") {
+    account.instalments = 6;
+    payer.plan = 172;
+  } else {
+    account.instalments = 12;
+    payer.plan = 86;
+  }
+  res.redirect('dates');
+});
+
+router.get('/payments/dd/contact-handler/', function (req, res) {
+  payer.hasText = false;
+  payer.hasEmail = false;
+  if (req.query.text == 'true') {
+    payer.hasText = true;
+  }
+  if (req.query.email == 'true') {
+    payer.hasEmail = true;
+  }
+  if (payer.hasText) {
+    res.redirect('text-value');
+  } else if (payer.hasEmail) {
+     res.redirect('mail-value');
+  } else {
+    payer.hasText = false;
+    payer.hasEmail = false;
+    res.redirect('accept');
+  }
+});
